21feecd084e83f7ff96884802844f841809c657b
AncientDaimyo <adfpbm@gmail.com>
2025-03-20 18:42:47 +0300

    Коммит перед слиянием репов

---
+++ src/Image/Service/ImageService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Image\Service;
+
+class ImageService
+{
+    
+}
---
+++ src/Image/Storage/ExtensionStorage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Image\Storage;
+
+class ExtensionStorage
+{
+    public const PNG = "png";
+    public const JPG = "jpg";
+    public const WEBP = "webp";
+}
