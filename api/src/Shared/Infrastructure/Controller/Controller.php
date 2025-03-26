3d27da231fb165f22e4f91304e8f65ddae846152
AncientDaimyo <adfpbm@gmail.com>
2025-03-26 19:53:41 +0300

    Метод для отправки ответа

--- api/src/Shared/Infrastructure/Controller/Controller.php
+++ api/src/Shared/Infrastructure/Controller/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Shared\Infrastructure\Controller;
 
+use JsonSerializable;
+use Slim\Psr7\Response;
 use Psr\Container\ContainerInterface;
 
 
@@ -13,5 +15,18 @@ abstract class Controller
     {
         $this->container = $container;
     }
-    
+
+
+    protected function sendResponse(
+        Response $response,
+        int $status = 200,
+        array|JsonSerializable $data = [],
+
+    ): Response {
+        if (!empty($data)) {
+            $response->getBody()->write(json_encode($data));
+        }
+
+        return $response->withStatus($status);
+    }
 }
